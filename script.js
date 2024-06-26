d7299c9b2213f0482d33c869138a9183124c6dfa
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2024-06-26 12:58:01 +0700

    add user input

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ import { config } from 'dotenv'
 config()
 
 import OpenAI from 'openai'
+import readline from 'readline'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
-const completion = await openai.chat.completions.create({
-  model: 'gpt-3.5-turbo-16k',
-  messages: [{ role: 'user', content: 'Hello!' }],
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 })
-console.log(completion.choices[0].message)
+
+userInterface.prompt()
+userInterface.on('line', async (input) => {
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-16k',
+    messages: [{ role: 'user', content: input }],
+  })
+  console.log(completion.choices[0].message)
+  userInterface.prompt()
+})
