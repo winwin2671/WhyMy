251d4616ce0b43f08ea7253f61a8ea1dc187d861
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2024-06-28 13:02:07 +0700

    added Assistants api

--- script.js
+++ script.js
@@ -15,12 +15,32 @@ const userInterface = readline.createInterface({
 
 userInterface.prompt()
 userInterface.on('line', async (input) => {
-  const completion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo-16k',
-    messages: [{ role: 'user', content: input }],
+  const assistant = await openai.beta.assistants.retrieve(
+    'asst_8gevepD9heOgP8wxYGiGl9rh',
+  )
+
+  // const thread = await openai.beta.threads.create()
+
+  const thread = await openai.beta.threads.retrieve(
+    'thread_GKcmu3oi0VCmagzIY87zgCpf',
+  )
+
+  const message = await openai.beta.threads.messages.create(thread.id, {
+    role: 'user',
+    content: input,
   })
 
-  var respond = completion.choices[0].message.content
-  console.log(respond)
+  let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+    assistant_id: assistant.id,
+  })
+
+  if (run.status === 'completed') {
+    const messages = await openai.beta.threads.messages.list(run.thread_id)
+    for (const message of messages.data.reverse()) {
+      console.log(`${message.role} > ${message.content[0].text.value}`)
+    }
+  } else {
+    console.log(run.status)
+  }
   userInterface.prompt()
 })
